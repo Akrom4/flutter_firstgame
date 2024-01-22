ff5ab712967e070ddb1c328ca3ca1af5094c058e
Fabrice Chaplain <75309565+Akrom4@users.noreply.github.com>
2024-01-22 16:39:19 +0100

    "Layout"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flame/game.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'klondike_game.dart';
 
 void main() {
-  final game= FlameGame();
+  WidgetsFlutterBinding.ensureInitialized();
+  final game = KlondikeGame();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((value) => runApp(GameWidget(game: game)));
+  
   runApp(GameWidget(game: game));
 }
